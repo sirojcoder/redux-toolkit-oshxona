2b9d09f448bb7b4a922b8b56a530d0729c7a6942
sirojcoder <sirojcoder@gmail.com>
2025-03-17 22:57:08 +0500

    up date 5

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,59 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login.jsx'
-import Oshpaz from './pages/oshpaz/Oshpaz.jsx'
-import RootGirgitton from './pages/girgitton/Root.jsx'
-import Menu from './pages/girgitton/Menu.jsx'
-import { Provider } from 'react-redux'
-import { store } from './store/Store.js'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import Login from './pages/Login.jsx'
+// import Oshpaz from './pages/oshpaz/Oshpaz.jsx'
+// import RootGirgitton from './pages/girgitton/Root.jsx'
+// import Menu from './pages/girgitton/Menu.jsx'
+// import { Provider } from 'react-redux'
+// import { store } from './store/Store.js'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Provider store={store}>
+//      <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<App />}>
+//         <Route path='/' element={<Login />}/>
+//             <Route path='girgitton' element={<RootGirgitton />} />
+//              <Route path='/client/:id' element={<Menu />} />
+//             <Route path='/oshpaz' element={<Oshpaz />}/> 
+//         </Route>
+//       </Routes>
+//      </BrowserRouter>
+//     </Provider>
+//   </StrictMode>,
+// )
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login.jsx';
+import Oshpaz from './pages/oshpaz/Oshpaz.jsx';
+import RootGirgitton from './pages/girgitton/Root.jsx';
+import Menu from './pages/girgitton/Menu.jsx';
+import { Provider } from 'react-redux';
+import { store } from './store/Store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />}>
-        <Route path='/' element={<Login />}/>
+      <BrowserRouter>
+        <Routes>
+          {/* Asosiy komponent App */}
+          <Route path='/' element={<App />}>
+            {/* Login sahifasi index sifatida qo‘shiladi */}
+            <Route index element={<Login />} />
             <Route path='girgitton' element={<RootGirgitton />} />
-             <Route path='/client/:id' element={<Menu />} />
-            <Route path='/oshpaz' element={<Oshpaz />}/> 
-        </Route>
-      </Routes>
-     </BrowserRouter>
+            <Route path='client/:id' element={<Menu />} />
+            <Route path='oshpaz' element={<Oshpaz />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
-)
+);
